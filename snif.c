60981616410080d2d50287cc12a931507e34cdae
KsushaVald <ksushavald@yandex.ru>
2017-07-24 21:11:13 +0700

    checksum

--- snif.c
+++ snif.c
@@ -15,6 +15,8 @@ struct my_ether{
 };
 struct my_ip{
 	u_int8_t ip_vhl;
+#define IP_V(ip)  (((ip)->ip_vhl & 0xf0)>>4)
+#define IP_HL(ip) ((ip)->ip_vhl & 0x0f)
 	u_int8_t ip_tos;
 	u_int16_t ip_len;
 	u_int16_t ip_id;
@@ -24,35 +26,31 @@ struct my_ip{
 	u_int16_t ip_sum;
 	struct in_addr ip_src, ip_dst;
 };
-u_int16_t checksum(u_char *ptr, int size){
+static u_int16_t checksum(u_int16_t *ptr,int size){
 	register long  sum=0;
 	while(size>1){
-		sum=sum+*ptr++;
-		size=size-sizeof(u_short);
+		sum+=* ptr++;
+		size-=sizeof(u_int16_t);
 	}
-	if(size>0){
-		sum=sum+((*ptr)&htons(0xFF00));;
+	if(size){
+		sum+=*(u_int16_t*)ptr;
 	}
-	while(sum>>16){
-		sum=(sum>>16)+(sum & 0xffff);
-	}
-	sum=~sum;
-	return ((u_int16_t)sum);
+	sum=(sum>>16)+(sum & 0xffff);
+	sum+=(sum>>16);
+	return ((u_int16_t)(~sum));
 }
 void my_ip_header(u_char *arg, struct pcap_pkthdr* pthdr, u_char *packet){
-	static int number=0; u_char *ptr; int size; u_int16_t  test;
-	struct my_ip *header, *for_check;
+	static int number=0;  u_int16_t  test=0;
+	struct my_ip *header;
 	header=(struct my_ip*)(packet+sizeof(struct my_ether));
-	for_check=(struct my_ip*)(packet+sizeof(struct my_ether));
-	ptr=(u_char*)for_check;
-	size=sizeof(for_check);
-	test=checksum(ptr,size);
+	test=checksum((u_int16_t*)header,IP_HL(header));
 	printf("%d: ",++number);
 	printf("IP-address destination:%s\n",inet_ntoa(header->ip_dst));
 	printf("IP-address sender:%s\n",inet_ntoa(header->ip_src));
 	printf("Protocol:%d\n",header->ip_p);
-	printf("Check summ:%d\n",htons(header->ip_sum));
-	printf("my check summ:%d\n",htons(test));
+	printf("HL IP:%d %d\n",IP_V(header),IP_HL(header));
+	printf("Check summ:%d\n",header->ip_sum);
+	printf("My check summ:%d\n",test);
 
 }
 void my_ether_header(u_char *arg, struct pcap_pkthdr* pthdr, u_char *packet){
